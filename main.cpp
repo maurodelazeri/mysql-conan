9a5f40825f33d5e85904c17086665dbe9abe4ab5
Mauro Delazeri <maurodelazeri@gmail.com>
2019-04-18 21:25:19 -0400

    update

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <mysqlx/xdevapi.h>
+
+using ::std::cout;
+using ::std::endl;
+
+int main()
+try {
+    using namespace ::mysqlx;
+
+    Session sess("user:password@host/schema");
+    Collection coll = sess
+            .getDefaultSchema()
+            .createCollection("myCollection", true);
+
+    coll.add(R"({ "name": "foo"; "age": 7 })").execute();
+
+    CollectionFind query = coll
+            .find("age < :age")
+            .fields("name", "age");
+
+    DocResult res = query.bind("age", 18).execute();
+
+    for (DbDoc doc : res)
+    {
+        cout << "name: " << doc["name"]
+             << ", age: " << doc["age"]
+             << endl;
+    }
+}
+catch (const mysqlx::Error &err)
+{
+    cout << "ERROR: " << err << endl;
+}
